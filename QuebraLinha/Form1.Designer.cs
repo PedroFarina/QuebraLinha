8ebb42ae76d3cab5bc8f1317bcc281fcf135029d
Pedro Farina <pedrogfarina@gmail.com>
2017-05-09 10:01:09 -0300

    Adicionando Projeto

---
+++ QuebraLinha/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace QuebraLinha
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            this.btnSelecionar = new System.Windows.Forms.Button();
+            this.lblQuant = new System.Windows.Forms.Label();
+            this.txtQuant = new System.Windows.Forms.TextBox();
+            this.dlgAbrir = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnSelecionar
+            // 
+            this.btnSelecionar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSelecionar.Location = new System.Drawing.Point(57, 51);
+            this.btnSelecionar.Name = "btnSelecionar";
+            this.btnSelecionar.Size = new System.Drawing.Size(146, 23);
+            this.btnSelecionar.TabIndex = 1;
+            this.btnSelecionar.Text = "Selecionar Arquivo";
+            this.btnSelecionar.UseVisualStyleBackColor = true;
+            this.btnSelecionar.Click += new System.EventHandler(this.btnSelecionar_Click);
+            // 
+            // lblQuant
+            // 
+            this.lblQuant.AutoSize = true;
+            this.lblQuant.Location = new System.Drawing.Point(12, 9);
+            this.lblQuant.Name = "lblQuant";
+            this.lblQuant.Size = new System.Drawing.Size(200, 13);
+            this.lblQuant.TabIndex = 2;
+            this.lblQuant.Text = "Quantidade de linhas para cada quebra :";
+            // 
+            // txtQuant
+            // 
+            this.txtQuant.Location = new System.Drawing.Point(15, 25);
+            this.txtQuant.Name = "txtQuant";
+            this.txtQuant.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtQuant.Size = new System.Drawing.Size(188, 20);
+            this.txtQuant.TabIndex = 3;
+            this.txtQuant.Text = "80000";
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(219, 87);
+            this.Controls.Add(this.txtQuant);
+            this.Controls.Add(this.lblQuant);
+            this.Controls.Add(this.btnSelecionar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmPrincipal";
+            this.Text = "Quebrador de Linhas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelecionar;
+        private System.Windows.Forms.Label lblQuant;
+        private System.Windows.Forms.TextBox txtQuant;
+        private System.Windows.Forms.OpenFileDialog dlgAbrir;
+    }
+}
+
---
+++ QuebraLinha/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace QuebraLinha
+{
+    public partial class frmPrincipal : Form
+    {
+        string Caminho, Nome;
+        int QuantLinhas;
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelecionar_Click(object sender, EventArgs e)
+        {
+            if(int.TryParse(txtQuant.Text, out QuantLinhas) == true)
+            {
+                dlgAbrir.ShowDialog();
+                if(dlgAbrir.FileName != "")
+                {
+                    string[] cut = dlgAbrir.FileName.Split('\\');
+                    foreach (string Item in cut)
+                    {
+                        if (Item != cut[cut.Length - 1])
+                        {
+                            Caminho += Item + @"\";
+                        }                       
+                    }
+                    Nome = cut[cut.Length - 1].Split('.')[0];
+                    Caminho += @"Quebrados";
+                    if (!Directory.Exists(Caminho))
+                    {
+                        Directory.CreateDirectory(Caminho);
+                    }
+                    Caminho += @"\";
+                    Quebrar();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Quantidade de linhas deve ser um número.", "Oops!");
+            }
+        }
+
+        private void Quebrar()
+        {
+            int i = 1, Contalinha = 0;
+            StreamWriter sw = new StreamWriter(Caminho + Nome + "(" + Convert.ToString(i) + ").txt", false);
+            foreach (string Linha in File.ReadLines(dlgAbrir.FileName, System.Text.Encoding.UTF8))
+            {
+
+                Contalinha += 1;
+                if ((Contalinha % QuantLinhas) == 0)
+                {
+                    sw.Write(Linha);
+                    sw.Close();
+                    i += 1;
+                    sw = new StreamWriter(Caminho + Nome + "(" + Convert.ToString(i) + ").txt", false);
+                }
+                else
+                {
+                    sw.WriteLine(Linha);
+                }
+            }
+            sw.Close();
+            MessageBox.Show("O arquivo foi quebrado.", "Sucesso!");
+            System.Diagnostics.Process.Start(Caminho);
+            Caminho = null;
+        }
+    }
+}
